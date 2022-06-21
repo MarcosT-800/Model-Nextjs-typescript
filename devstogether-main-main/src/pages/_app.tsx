5f2ca4d8c5ab209176875805379b04069fc794f9
Marcos Alves <82832426+MarcosT-800@users.noreply.github.com>
2022-06-21 15:25:39 -0300

    Add files via upload

---
+++ devstogether-main-main/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/globals';
+import '../styles/globals.ts';
+import theme from '../styles/theme';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
